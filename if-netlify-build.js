5e200504da278c92ee4a77df05cfe5044dd7309e
roblotter <roblotter1@gmail.com>
2019-11-22 17:43:56 +0200

    update

--- if-netlify-build.js
+++ if-netlify-build.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
 if (process.env.NETLIFY_BUILD == "true") {
+  console.log("building on netlify");
   require("fs").writeFileSync(
     require("path").resolve(__dirname, ".npmrc"),
     "\n@adaptabletools:registry=https://registry.adaptabletools.com\n//registry.adaptabletools.com/:_authToken=${NPM_TOKEN}"
   );
+} else {
+  console.log("Local instal...");
 }
