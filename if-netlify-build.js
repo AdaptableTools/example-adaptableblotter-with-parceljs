7b9e271a2b98156e54b480678efd79179442d2ee
roblotter <roblotter1@gmail.com>
2019-11-22 17:47:04 +0200

    update

--- if-netlify-build.js
+++ if-netlify-build.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+const { execSync } = require("child_process");
+execSync(
+  "npm config set @adaptabletools:registry https://registry.adaptabletools.com/"
+);
 if (process.env.NETLIFY_BUILD == "true") {
   console.log("building on netlify");
   require("fs").writeFileSync(
